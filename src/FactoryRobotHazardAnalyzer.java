6c41cd175f8aaeca36110778a56f3ac0212aed60
komal624 <kb4858@srmist.edu.in>
2026-01-29 22:35:57 +0530

    [Komal] : Added UC4 - Validation Using If-Else

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -15,11 +15,17 @@ public class FactoryRobotHazardAnalyzer {
         System.out.print("Enter Worker Density: ");
         int workerDensity = scanner.nextInt();
 
-        // UC3: Risk Calculation (No Validation)
-        double hazardRisk = armPrecision * workerDensity;
-
-        // Output
-        System.out.println("Hazard Risk Score: " + hazardRisk);
+        // UC4: Validation using if-else
+        if (armPrecision <= 0 || armPrecision > 100) {
+            System.out.println("Invalid Arm Precision. Must be between 1 and 100.");
+        }
+        else if (workerDensity < 0 || workerDensity > 500) {
+            System.out.println("Invalid Worker Density. Must be between 0 and 500.");
+        }
+        else {
+            double hazardRisk = armPrecision * workerDensity;
+            System.out.println("Hazard Risk Score: " + hazardRisk);
+        }
 
         scanner.close();
     }
