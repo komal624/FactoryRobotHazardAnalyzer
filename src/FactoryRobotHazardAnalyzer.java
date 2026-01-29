21a895a8caa7aba6360fb93b6de824ed56bc1815
komal624 <kb4858@srmist.edu.in>
2026-01-29 22:39:24 +0530

    [Komal] : Added UC5 - Refactored Logic into Method

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -15,18 +15,29 @@ public class FactoryRobotHazardAnalyzer {
         System.out.print("Enter Worker Density: ");
         int workerDensity = scanner.nextInt();
 
-        // UC4: Validation using if-else
-        if (armPrecision <= 0 || armPrecision > 100) {
-            System.out.println("Invalid Arm Precision. Must be between 1 and 100.");
-        }
-        else if (workerDensity < 0 || workerDensity > 500) {
-            System.out.println("Invalid Worker Density. Must be between 0 and 500.");
-        }
-        else {
-            double hazardRisk = armPrecision * workerDensity;
+        // UC5: Call to business logic method
+        double hazardRisk = calculateHazardRisk(armPrecision, workerDensity);
+
+        if (hazardRisk != -1) {
             System.out.println("Hazard Risk Score: " + hazardRisk);
         }
 
         scanner.close();
+    }
+
+    // Business logic + validation
+    public static double calculateHazardRisk(double armPrecision, int workerDensity) {
+
+        if (armPrecision <= 0 || armPrecision > 100) {
+            System.out.println("Invalid Arm Precision. Must be between 1 and 100.");
+            return -1;
+        }
+
+        if (workerDensity < 0 || workerDensity > 500) {
+            System.out.println("Invalid Worker Density. Must be between 0 and 500.");
+            return -1;
+        }
+
+        return armPrecision * workerDensity;
     }
 }
